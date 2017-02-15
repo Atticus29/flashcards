783a76bf31f6ae7e0cc6760002997ae8ff6fe23d
Mark Fisher and Xi Xia <xi@>
2017-02-14 16:53:13 -0800

    add images upon click of flashcard list items

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,25 @@ $(document).ready(function() {
     // (".defintionContent").find($(this).text()).css("color", "red");
     $(".defintionContent").removeClass("activeDef");
     var defOfInterest = "#" + $(this).text(); //defOfInterest is now a string
-
     $(defOfInterest).addClass("activeDef"); //selecting thing that matches string value
+    // console.log(defOfInterest);
     // .css("color", "red");
-    $("h2").remove();
-    $("div#definitionHeading").prepend("<h2>"+$(this).text()+"</h2>");
+    // $("h2").remove();
+    // $("div#definitionHeading").prepend("<h2>"+$(this).text()+"</h2>");
+    $("div#definitionHeading").text($(this).text());
+
+    if ($(this).text() ==="JavaScript"){
+      $("#javaImage").addClass("activeDef");
+    }
+
+    if ($(this).text() ==="Operators"){
+      $("#operatorImage").addClass("activeDef");
+    }
+
+    if ($(this).text() ==="Variables"){
+      $("#variables").addClass("activeDef");
+    }
+
   });
   $("button#invertButton").click(function(){
     $("body").toggleClass("darkBody");
